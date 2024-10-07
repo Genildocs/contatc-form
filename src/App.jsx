5df0c137b3ab0f33e41c587e784f3de9e3cf42b6
genildocs <genildocs@gmail.com>
2024-10-07 06:12:08 -0300

    implement ract toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Form from './components/Form.jsx';
-
+import Completed from './components/Completed.jsx';
 export default function App() {
   return (
     <div className=" mx-3 my-8 sm:flex justify-center">
-      <Form />
+      <Completed />
     </div>
   );
 }
--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -1,4 +1,16 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function Completed({ valid }) {
-  console.log(valid);
-  return <div>{valid && <div>STATUS COMPLETED</div>}</div>;
+  const notify = () => toast('Formulario enviado com sucesso!');
+  return (
+    <div>
+      {' '}
+      <div>
+        <button onClick={notify}>Submit</button>
+        <ToastContainer />
+      </div>
+    </div>
+  );
 }
